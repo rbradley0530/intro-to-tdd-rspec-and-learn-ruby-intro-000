2c05a60bcb35dfaf7a12cea20ac72d0e03e2f6b7
Ryan Bradley <ryanbradley@kpmg.com>
2019-01-29 00:55:12 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -4,6 +4,7 @@ describe "current_age_for_birth_year method" do
   it "returns the age of a person based on the year of birth" do
 
     def current_age_for_birth_year
+    end
 
     age_of_person = current_age_for_birth_year(1984)
 
